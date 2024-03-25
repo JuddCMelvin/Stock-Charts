a1e3c74237a00d41eb47c8d155dfb9c82ef2b9db
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-03-24 20:44:02 -0400

    seems to be connected and pulling date

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
 async function main() {
 
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+    // const timeChartCanvas = document.querySelector('#time-chart');
+    // const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
+    // const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    const url = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MFST,DIS,BNTX&interval=1min&apikey=f0ca8536deb943beb424180a442fe1af')
+    const data = await url.json()
+    console.log(data)
 }
+
 
 main()
