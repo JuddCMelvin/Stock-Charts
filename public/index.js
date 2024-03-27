4490bdfe1c5b9b9da315eef8e35f9fa8494d3935
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-03-27 19:00:42 -0400

    trying to get chart to show up

--- public/index.js
+++ public/index.js
@@ -5,8 +5,31 @@ async function main() {
     // const averagePriceChartCanvas = document.querySelector('#average-price-chart');
     const url = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MFST,DIS,BNTX&interval=1min&apikey=f0ca8536deb943beb424180a442fe1af')
     const data = await url.json()
-    console.log(data)
+    
+    const { GME, MSFT, DIS, BNTX } = mockData;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+    
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor:  'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)'
+            }]
+        }
+    });
+
+    console.log(stocks[0].values)
 }
 
+// Bonus Note: 
+// Another way to write the above lines would to refactor it as:
+   // const {GME, MSFT, DIS, BTNX} = result 
+// This is an example of "destructuring" an object
+// "Destructuring" creates new variables from an object or an array
 
-main()
+                                                
