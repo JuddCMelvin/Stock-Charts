93cc2946caf8f8f7cdc2d2211648703703dcadea
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-03-27 22:13:53 -0400

    added new chart

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
 async function main() {
 
-    // const timeChartCanvas = document.querySelector('#time-chart');
-    // const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    // const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-    const url = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MFST,DIS,BNTX&interval=1min&apikey=f0ca8536deb943beb424180a442fe1af')
-    const data = await url.json()
+    const timeChartCanvas = document.querySelector('#time-chart');
+    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
+    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    // const url = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MFST,DIS,BNTX&interval=1min&apikey=f0ca8536deb943beb424180a442fe1af')
+    // const data = await url.json()
     
     const { GME, MSFT, DIS, BNTX } = mockData;
 
@@ -22,6 +22,7 @@ async function main() {
             }]
         }
     });
+    
 
     console.log(stocks[0].values)
 }
